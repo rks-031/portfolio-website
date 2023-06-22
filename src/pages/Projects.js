8d3bbb41959ee46da2dc736d5808e34a89f3e689
Rajnish <b421037@iiit-bh.ac.in>
2023-06-22 16:20:36 +0530

    images and box-shadow added to cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -25,11 +25,7 @@ export default function Projects() {
       <div class="row row-cols-3 g-4">
         <div class="col">
           <div class="card my-3 card-hover">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-              class="card-img-top"
-              alt="Hollywood Sign on The Hill"
-            />
+            <img src="/assets/meme.jpg" class="card-img-top" alt="meme" />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">memeGenerator</h5>
               <p class="card-text">
@@ -37,6 +33,8 @@ export default function Projects() {
                 ReactJS which allows users to generate and customize memes by
                 adding text captions to images.
               </p>
+              <br />
+              <br />
               <a
                 href="https://github.com/rks-031/memeGenerator.git"
                 class="btn button-color"
@@ -49,11 +47,7 @@ export default function Projects() {
         </div>
         <div class="col">
           <div class="card my-3 card-hover">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
-              class="card-img-top"
-              alt="Palm Springs Road"
-            />
+            <img src="/assets/pattern.png" class="card-img-top" alt="pattern" />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">patternPrinting</h5>
               <p class="card-text">
@@ -61,6 +55,7 @@ export default function Projects() {
                 which can be very essential for placements. The tech stack used
                 for programming here is Java.
               </p>
+              <br />
               <a
                 href="https://github.com/rks-031/patternPrinting.git"
                 class="btn button-color"
@@ -74,9 +69,9 @@ export default function Projects() {
         <div class="col">
           <div class="card my-3 card-hover">
             <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
+              src="/assets/overview.jpg"
               class="card-img-top"
-              alt="Los Angeles Skyscrapers"
+              alt="overview"
             />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">rks-031</h5>
@@ -85,6 +80,8 @@ export default function Projects() {
                 a user's Github overview page. You can just simply copy down the
                 code and personalize it with your own data.
               </p>
+              <br />
+              <br />
               <a
                 href="https://github.com/rks-031/rks-031.git"
                 class="btn button-color"
@@ -98,18 +95,19 @@ export default function Projects() {
         <div class="col">
           <div class="card my-3 card-hover">
             <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
+              src="/assets/jwellery.jpg"
               class="card-img-top"
-              alt="Skyscrapers"
+              alt="jwellery"
             />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">SRJ-Jwellery</h5>
               <p class="card-text">
                 This is a small demo project created using react made for a
                 jwellery shop/firm which can be used for advertising/promoting
-                its products to the customers and giving them a brief idea and
-                description about the product.
+                its products to the customers and decribing about the same.
               </p>
+              <br />
+              <br />
               <a
                 href="https://github.com/rks-031/SRJ-Jwellery.git"
                 class="btn button-color"
@@ -122,19 +120,16 @@ export default function Projects() {
         </div>
         <div class="col">
           <div class="card my-3 card-hover">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/046.webp"
-              class="card-img-top"
-              alt="Skyscrapers"
-            />
+            <img src="/assets/weather.jpg" class="card-img-top" alt="weather" />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">weatherApp</h5>
               <p class="card-text">
-                A weather app created using Node.js is a web application that
-                provides real-time weather information to users.The app
-                integrates with a weather data provider's API: OpenWeatherMap.
-                Tech Stack used here is Node.js
+                A weather app is a web application that provides real-time
+                weather information to users.The app integrates with a weather
+                data provider's API: OpenWeatherMap.Tech Stack used: Node.js
               </p>
+              <br />
+              <br />
               <a href="#" class="btn button-color" target="_blank">
                 Github Repository
               </a>
@@ -143,19 +138,17 @@ export default function Projects() {
         </div>
         <div class="col">
           <div class="card my-3 card-hover">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp"
-              class="card-img-top"
-              alt="Skyscrapers"
-            />
+            <img src="/assets/form.png" class="card-img-top" alt="form" />
             <div class="card-body">
               <h5 class="card-title font-weight-bold">dynamicForm</h5>
               <p class="card-text">
-                This is a demo of a basic dynamic form created using ReactJs
-                which can be embedded/used directly into various kind of
-                websites irrespective of the purpose and can be modified as per
-                required. It's a very basic app and can be scaled.
+                Demo of a dynamic form created using ReactJs which can be
+                embedded/used directly into various kind of websites
+                irrespective of the purpose and can be modified as per required.
+                It's a very basic app and can be scaled.
               </p>
+              <br />
+              <br />
               <a
                 href="https://github.com/rks-031/dynamicForm.git"
                 class="btn button-color"
