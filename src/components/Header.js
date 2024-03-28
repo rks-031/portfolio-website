6118c1376e740e3a31175a5059c06a82c17038d9
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-03-28 21:11:57 +0530

    rks logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
       <nav
-        className="navbar navbar-expand-lg navbar-light fixed-top pt-3 pb-3"
+        className="navbar navbar-expand-lg navbar-light bg-light fixed-top pt-3 pb-3"
         style={{ background: "linear-gradient(to bottom, #141e30, #243b55)" }}
       >
-        <button className="navbar-brand mx-3 header-name">
-          <a href="/">RKS</a>
-        </button>
+        <a href="/" className="navbar-brand mx-3">
+          <img
+            src="./assets/white-logo.svg"
+            alt="rks"
+            style={{ height: "40px", width: "35px" }}
+          />
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -24,17 +29,17 @@ export default function Header() {
         <div className="collapse navbar-collapse " id="navbarNav">
           <ul className="navbar-nav ml-auto mx-3 links">
             <li className="nav-item active">
-              <a className="nav-link text-white" href="/about">
+              <a className="nav-link text-white mx-3" href="/about">
                 About Me
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link text-white" href="/projects">
+              <a className="nav-link text-white mx-3" href="/projects">
                 Projects
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link text-white" href="/contact">
+              <a className="nav-link text-white mx-3" href="/contact">
                 Contact Me
               </a>
             </li>
